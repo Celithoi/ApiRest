a2c9255413f2ef6e60ca9d9007f194c3cb80745a
Marcelo Henrique Rocha Cardoso <100224714+Celithoi@users.noreply.github.com>
2025-04-07 19:41:24 -0300

    aceitando foto dos alunos

--- src/controllers/fotoController.js
+++ src/controllers/fotoController.js
@@ -1,7 +1,17 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig.js';
+const upload = multer(multerConfig).single('foto');
 
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [errors.code],
+        });
+      }
+      return res.json(req.file);
+    });
 
   }
 }
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import FotoController from '../controllers/fotoController.js';
-import multerConfig from '../config/multerConfig.js';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), FotoController.store);
+router.post('/', FotoController.store);
 
 export default router;
